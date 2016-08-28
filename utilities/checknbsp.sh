56a938cedbd3f3d66edb9238957057d51b147d2e
Akira Yokosawa <akiyks@gmail.com>
2016-08-28 23:58:51 +0900

    Add scripts for adding non-breaking spaces
    Signed-off-by: Akira Yokosawa <akiyks@gmail.com>

---
+++ utilities/checknbsp.sh
@@ -0,0 +1,62 @@
+#!/bin/sh
+#
+# Add necessary nbsps around numbers
+#
+# Replacement candidates are
+# nn byte -> nn~byte
+# Line nn -> Line~nn
+# line nn -> line~nn
+# Lines nn -> Lines~nn
+# lines nn -> lines~nn
+# CPU nn -> CPU~nn
+# Thread nn -> Thread~nn
+# and nn -> and~nn
+# through nn -> through~nn
+# ...
+#
+# Several rules at the end are to revert inappropriate changes
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, you can access it online at
+# http://www.gnu.org/licenses/gpl-2.0.html.
+#
+# Copyright (C) Akira Yokosawa, 2016
+#
+# Authors: Akira Yokosawa <akiyks@gmail.com>
+
+cat $1 |
+	sed -e 's/\([0-9]\+\) byte/\1~byte/g' \
+	    -e 's/Line \([0-9]\+\)/Line~\1/g' \
+	    -e 's/line \([0-9]\+\)/line~\1/g' \
+	    -e 's/Lines \([0-9]\+\)/Lines~\1/g' \
+	    -e 's/lines \([0-9]\+\)/lines~\1/g' \
+	    -e "s/CPU \([0-9]\+[,\.\']\?[[:space:]]\+\)/CPU~\1/g" \
+	    -e 's/Thread \([0-9]\+\)/Thread~\1/g' \
+	    -e "s/and \([0-9]\+[,\.\']\?[[:space:]]\+\)/and~\1/g" \
+	    -e "s/and \([0-9]\+[,\.\']\?\)$/and~\1/g" \
+	    -e "s/through \([0-9]\+[,\.\']\?[[:space:]]\+\)/through~\1/g" \
+	    -e "s/through \([0-9]\+[,\.\']\?\)$/through~\1/g" \
+	    -e 's/Figure \(\\ref{.*}\)/Figure~\1/g' \
+	    -e 's/Table \(\\ref{.*}\)/Table~\1/g' \
+	    -e 's/Chapter \(\\ref{.*}\)/Chapter~\1/g' \
+	    -e 's/Section \(\\ref{.*}\)/Section~\1/g' \
+	    -e 's/\\co{CPU~\([0-9]\+\)}/\\co{CPU \1}/g' \
+	    -e 'sX/\* Line~\([0-9]\+\)X/* Line \1Xg' \
+	    -e 'sX/\* Lines~\([0-9]\+\)X/* Lines \1Xg' \
+	    -e 'sX/\* \(.*\) and~\([0-9]\+\) \*/X/* \1 and \2 */Xg' \
+	    -e 's/^\(.* proc .* line\)~\([0-9]\+ .*\)$/\1 \2/g' \
+	    -e 's/^\(.*line\)~\([0-9]\+.* "-end-"\)$/\1 \2/g' \
+	    -e 's/\([0-9]\+\)~byte \(.*\);$/\1 byte \2;/g' \
+	    -e 's/^\(State-vector [0-9]\+\)~\(byte, depth reached .*\)$/\1 \2/g' \
+
+#	    -e 's/\\co{\(.*CPU\)~~\([0-9]\+.*\)}/\\co{\1 \2}/g' \
