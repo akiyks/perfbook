f4a3bac0342ca49efeed158f48a3e4f3a94dd808
Akira Yokosawa <akiyks@gmail.com>
2017-02-01 19:35:00 +0900

    Add --endash option to autodate.sh
    By giving "--endash" option to autodate.sh, dohyphen2endash.sh
    is invoked before output to stdout.
    
    Signed-off-by: Akira Yokosawa <akiyks@gmail.com>

--- utilities/autodate.sh
+++ utilities/autodate.sh
@@ -4,6 +4,8 @@
 # If invoked on not-clean git repository, append "(m)" to date field
 # for title.
 # If git status is not availabe, use current date instead.
+# If --endash option is given,
+# call utilities/dohyphen2endash.sh on clean git repository.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -23,12 +25,29 @@
 #
 # Authors: Akira Yokosawa <akiyks@gmail.com>
 
+# parse option
+TEMP=`getopt -o e --long endash -- "$@"`
+
+if [ $? != 0 ] ; then echo "Error in parse option..." >&2 ; exit 1 ; fi
+
+eval set -- "$TEMP"
+
+doendash=0
+
+while true ; do
+	case "$1" in
+		-e|--endash) doendash=1 ; shift ;;
+		--) shift ; break ;;
+		*) echo "Unknown option!" ; exit 1 ;;
+	esac
+done
+
 export LC_TIME=C
 # check if we are in git repository
 if ! test -e .git
 then
 	date_str=`date -R`
-	modified=""
+	modified="(u)"
 else
 	date_str=`git show --format="%cD" | head -1`
 	# check if git status is clean
@@ -44,5 +63,20 @@ month=`date --date="$date_str" +%B`
 year=`date --date="$date_str" +%Y`
 day=`date --date="$date_str" +%e`
 
-env printf "\\date{%s %s, %s %s}\n" $month $day $year $modified
+# call dohyphen2endash if not modified and --endash is given
+if [ $doendash -eq 1 ]
+then
+	if [ -z "$modified" ]
+	then
+		echo "replacing hyphens to endashes" 1>&2
+		sh utilities/dohyphen2endash.sh > /dev/null
+		modified="(--)"
+	else
+		echo "git status not clean, skipping endash replacement" 1>&2
+	fi
+fi
+
+env printf "\\date{%s %s, %s \\scriptsize{%s}}\n" $month $day $year $modified
 env printf "\\\newcommand{\\\commityear}{%s}\n" $year
+
+exit 0
