bf3aee17002dc4126b3bb9d151d08c6321550da7
Akira Yokosawa <akiyks@gmail.com>
2018-11-05 07:51:04 +0900

    styleguide: Update Code Snippet section
    Signed-off-by: Akira Yokosawa <akiyks@gmail.com>

---
+++ appendix/styleguide/samplecodesnippet.c
@@ -0,0 +1,11 @@
+//\begin{snippet}[labelbase=ln:app:styleguide:samplecodesnippet:main,commandchars=\$\[\]]
+/*
+ * Sample Code Snippet
+ */
+#include <stdio.h>
+int main(void)
+{
+	printf("Hello world!\n");	//\lnlbl{printf}
+	return 0;			//\lnlbl{return}
+}
+//\end{snippet}
