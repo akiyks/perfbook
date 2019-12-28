9ca300c2fa1e730fd8e1918f6021e480682c48e4
Akira Yokosawa <akiyks@gmail.com>
2019-12-28 20:23:19 +0900

    utilities/torelease: Make sure new tag is pushed
    Also use variables "repo_url" and "remote" to make it possible to
    use an alternative repository for contingency.
    
    Also fix several bugs:
        - Invert clean status check.
        - Ignore untracked files.
        - Existing tag check, which always failed due to the regex
          pattern given in single quotes.
    
    Other unnecessary quotation marks around variable/command evaluations
    have been removed.
    
    Pushing the new tag will silence a warning from "git request-pull"
    saying that the matching commit could not be found in the remote
    repository.
    
    Signed-off-by: Akira Yokosawa <akiyks@gmail.com>

--- utilities/torelease.sh
+++ utilities/torelease.sh
@@ -3,11 +3,13 @@
 # torelease.sh: Produce release work products for perfbook, including
 #	PDFs and changelog.
 #
-# Usage torelease.sh [ destdir [ "Edition tag" ] ]
+# Usage torelease.sh [ destdir [ "Edition tag" [ repo_url [ remote ] ] ] ]
 #
 # The destination directory defaults to /tmp, and the edition tag
 # defaults to being a release tag, as in v2019.12.22a.  Edition tags
 # have the form "Edition.1", "Edition.1P", or "Edition.1P-rc3".
+# "repo_url" defaults to Paul's git repository at gitolite.kernel.org.
+# "remote" defaults to "origin".
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -27,28 +29,29 @@
 #
 # Authors: Paul E. McKenney <paulmck@kernel.org>
 
-destdir="${1-/tmp}"
-tag="${2-`date +%Y.%m.%d`a}"
-
-if ! test -d "${destdir}" -o ! -w "${destdir}"
+destdir=${1-/tmp}
+tag=${2-`date +%Y.%m.%d`a}
+repo_url=${3-git@gitolite.kernel.org:pub/scm/linux/kernel/git/paulmck/perfbook.git}
+remote=${4-origin}
+if ! test -d ${destdir} -o ! -w ${destdir}
 then
 	echo Bogus destination directory, giving up.
 	exit 1
 fi
-if ! git status --porcelain > /dev/null 2>&1
+if ! git status -uno --porcelain > /dev/null 2>&1
 then
 	echo Git repository nonexistent or in bad shape, giving up.
 	exit 2
 fi
-if test -n "`git status --porcelain`"
+if test `git status --porcelain`
 then
 	git status
 	echo Git repository working tree not clean, giving up.
 	exit 3
 fi
 
-oldtag="`git describe --tags HEAD | sed -e 's/-.*$//'`"
-case "$t{tag}" in
+oldtag=`git describe --tags HEAD | sed -e 's/-.*$//'`
+case ${tag} in
 [0-9]*)
 	gittag=v${tag}
 	;;
@@ -56,38 +59,44 @@ case "$t{tag}" in
 	gittag=${tag}
 	;;
 esac
-if git tag -l | grep -q '^${gittag}$'
+if git tag -l | grep -q \^${gittag}\$
 then
 	echo Tag ${gittag} already exists, giving up.
 	exit 4
 fi
-if ! git tag "${gittag}"
+if ! git tag ${gittag}
 then
-	echo "Giving up."
+	echo Giving up.
 	exit 5
 fi
 touch perfbook.tex # Force re-run of "utilities/autodate.sh"
 
 if ! make
 then
-	git tag -d "${gittag}"
+	git tag -d ${gittag}
 	echo Double-column build failed, giving up.
 	exit 6
 fi
-cp perfbook.pdf "${destdir}/perfbook.${tag}.pdf"
+cp perfbook.pdf ${destdir}/perfbook.${tag}.pdf
 
 if ! make 1c
 then
-	git tag -d "${gittag}"
+	git tag -d ${gittag}
 	echo Single-column build failed, giving up.
 	exit 7
 fi
-cp perfbook.pdf "${destdir}/perfbook-1c.${tag}.pdf"
+cp perfbook-1c.pdf ${destdir}/perfbook-1c.${tag}.pdf
 
 # Yes, this will ask for credentials for the remote repository...
 # If this becomes too irritating, a replacement script can be created.
-git request-pull $oldtag git@gitolite.kernel.org:pub/scm/linux/kernel/git/paulmck/perfbook.git | sed -n '/^--*$/,$p' | tail +2 > "${destdir}/Changes.${tag}.txt"
+if ! git push ${remote} ${gittag}
+then
+	git tag -d ${gittag}
+	echo Tag push failed, giving up.
+	exit 8
+fi
+git request-pull ${oldtag} ${repo_url} ${gittag} | sed -n '/^--*$/,$p' | tail +2 > ${destdir}/Changes.${tag}.txt
 
-ls -l "${destdir}/perfbook.${tag}.pdf" "${destdir}/perfbook-1c.${tag}.pdf" "${destdir}/Changes.${tag}.txt"
+ls -l ${destdir}/perfbook.${tag}.pdf ${destdir}/perfbook-1c.${tag}.pdf ${destdir}/Changes.${tag}.txt
 echo Release v${tag} prepared in ${destdir}
 exit 0
