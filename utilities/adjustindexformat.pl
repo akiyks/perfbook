461cdcb76b6f761fc12cde16f9dd674d3f6bf2ec
Akira Yokosawa <akiyks@gmail.com>
2022-01-08 20:30:39 +0900

    Initial indexformat support
    Existing indexing scheme conflicts with makeindex's native
    page number formatting.
    Use a helper script to overcome the conflict.
    
    Not-Signed-off-by: Akira Yokosawa <akiyks@gmail.com>

---
+++ utilities/adjustindexformat.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/env perl
+# SPDX-License-Identifier: GPL-2.0-or-later
+
+use strict;
+use warnings;
+
+my $line;
+
+open(my $fh, '<:encoding(UTF-8)', $ARGV[0])
+    or die "Could not open file '$ARGV[0]' $!";
+
+while($line = <$fh>) {
+    $line =~ s/\{([^\|]+)(\|hyperindexformat)\{\\bf(@\\makefirstuc )\{.+\}\}\}/\{$1$3\{$1\}$2\{\\BF\}\}/ ;
+    $line =~ s/\{([^\|]+)(\|hyperindexformat)\{\\bf(@\\makefirstuc )\{.+\}!([^\}]+)\}\}/\{$1$3\{$1}!$4$2\{\\BF\}\}/ ;
+    $line =~ s/(\\makefirstuc )\{([^\)]+)\} \[([^\]]+)\]\|hyperpage\}/$1\{$2\} \($3\)|hyperindexformat\{\\BF\}\}/ ;
+    print $line ;
+}
