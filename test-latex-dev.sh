cee932d2c686ab1222cee1997be4b3f5081b8df4
Akira Yokosawa <akiyks@gmail.com>
2025-06-08 19:18:50 +0900

    test-latex-dev.sh: Don't run latex commands if unavailable
    Signed-off-by: Akira Yokosawa <akiyks@gmail.com>

--- test-latex-dev.sh
+++ test-latex-dev.sh
@@ -1,11 +1,24 @@
 #! /bin/bash -f
 
 NPROC=`nproc --all`
+PDFLATEX_CMD=`command -v pdflatex`
+PDFLATEX_DEV_CMD=`command -v pdflatex-dev`
+LUALATEX_CMD=`command -v lualatex`
+LUALATEX_DEV_CMD=`command -v lualatex-dev`
+#
 make -j $NPROC contrib.tex origpub.tex sub_qqz
 #
-make LATEX=pdflatex 2c &
-make LATEX=pdflatex-dev 1c &
-make LATEX=lualatex eb &
-make LATEX=lualatex-dev ebsf &
+if [ $PDFLATEX_CMD != "" ] ; then
+	make LATEX=pdflatex 2c &
+fi
+if [ $PDFLATEX_DEV_CMD != "" ] ; then
+	make LATEX=pdflatex-dev 1c &
+fi
+if [ $LUALATEX_CMD != "" ] ; then
+	make LATEX=lualatex eb &
+fi
+if [ $LUALATEX_DEV_CMD != "" ] ; then
+	make LATEX=lualatex-dev ebsf &
+fi
 #
 wait
