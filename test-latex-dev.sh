c7765bb72acfb6278eed57722e8acb960da2e2dd
Akira Yokosawa <akiyks@gmail.com>
2025-06-09 16:15:10 +0900

    test-latex-dev.sh: Use switch/case for available latex engines
    Signed-off-by: Akira Yokosawa <akiyks@gmail.com>

--- test-latex-dev.sh
+++ test-latex-dev.sh
@@ -8,17 +8,53 @@ LUALATEX_DEV_CMD=`command -v lualatex-dev`
 #
 make -j $NPROC contrib.tex origpub.tex sub_qqz
 #
-if [ $PDFLATEX_CMD != "" ] ; then
-	make LATEX=pdflatex 2c &
+LATEX_ENGINES=""
+if [ "x$PDFLATEX_CMD" = "x" ] ; then
+	LATEX_ENGINES="n$LATEX_ENGINES"
+else
+	LATEX_ENGINES="y$LATEX_ENGINES"
 fi
-if [ $PDFLATEX_DEV_CMD != "" ] ; then
-	make LATEX=pdflatex-dev 1c &
+if [ "x$PDFLATEX_DEV_CMD" = "x" ] ; then
+	LATEX_ENGINES="n$LATEX_ENGINES"
+else
+	LATEX_ENGINES="y$LATEX_ENGINES"
 fi
-if [ $LUALATEX_CMD != "" ] ; then
-	make LATEX=lualatex eb &
+if [ "x$LUALATEX_CMD" = "x" ] ; then
+	LATEX_ENGINES="n$LATEX_ENGINES"
+else
+	LATEX_ENGINES="y$LATEX_ENGINES"
 fi
-if [ $LUALATEX_DEV_CMD != "" ] ; then
-	make LATEX=lualatex-dev ebsf &
+if [ "x$LUALATEX_DEV_CMD" = "x" ] ; then
+	LATEX_ENGINES="n$LATEX_ENGINES"
+else
+	LATEX_ENGINES="y$LATEX_ENGINES"
 fi
+
+echo "latex engines: $LATEX_ENGINES"
+
+case $LATEX_ENGINES in
+	"yyyy")
+		make LATEX=pdflatex 2c &
+		make LATEX=pdflatex-dev 1c &
+		make LATEX=lualatex eb &
+		make LATEX=lualatex-dev ebsf &
+		;;
+	"nyny")
+		make LATEX=pdflatex 2c &
+		make LATEX=pdflatex 1c &
+		make LATEX=lualatex eb &
+		make LATEX=lualatex ebsf &
+		;;
+	"nnyy")
+		make LATEX=pdflatex 2c &
+		make LATEX=pdflatex-dev 1c &
+		make LATEX=pdflatex eb &
+		make LATEX=pdflatex-dev ebsf &
+		;;
+	*)
+		echo "Unexpected list of availabe latex engines"
+		exit 1
+
+	esac
 #
 wait
